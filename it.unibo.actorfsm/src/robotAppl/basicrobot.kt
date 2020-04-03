c66055c04182cfe15ac7603281f6a573ee7981b2
Antonio Natali <antonio.natali@unibo.it>
2020-04-03 19:17:18 +0200

    qak intro

--- it.unibo.actorfsm/src/robotAppl/basicrobot.kt
+++ it.unibo.actorfsm/src/robotAppl/basicrobot.kt
@@ -50,8 +50,8 @@ class basicrobot ( name: String, scope: CoroutineScope,
 					//println("$ndnt basicrobot | waits ...")  
  				}
 				transition( edgeName="t0",targetState="handlesensor", cond=whenDispatch("sensor") )				
-				transition( edgeName="t1",targetState="endwork", cond=whenDispatch("end") )				
-				transition( edgeName="t2",targetState="execcmd", cond=whenDispatch("cmd") )				
+				transition( edgeName="t1",targetState="endwork",      cond=whenDispatch("end")    )				
+				transition( edgeName="t2",targetState="execcmd",      cond=whenDispatch("cmd")    )				
 			}
 			state("execcmd"){
 				action { //it:State
