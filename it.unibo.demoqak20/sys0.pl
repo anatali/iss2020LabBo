3f0227a6f37aaaa83619fdef14d326c636a93340
anatalibo <antonio.natali@unibo.it>
2020-04-06 17:13:27 +0200

    week7

--- it.unibo.demoqak20/sys0.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% sys0 description   
-%====================================================================================
-mqttBroker("localhost", "1883").
-context(ctx1, "localhost",  "TCP", "8010").
- qactor( qak1, ctx1, "it.unibo.qak1.Qak1").
-  qactor( qak2, ctx1, "it.unibo.qak2.Qak2").
-tracing.
