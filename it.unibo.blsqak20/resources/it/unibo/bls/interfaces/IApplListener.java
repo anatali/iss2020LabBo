108e8b3d12e1f274740b677efb38769fd3917d6b
anatalibo <antonio.natali@unibo.it>
2020-04-18 17:26:19 +0200

    blsintro

---
+++ it.unibo.blsqak20/resources/it/unibo/bls/interfaces/IApplListener.java
@@ -0,0 +1,9 @@
+package it.unibo.bls.interfaces;
+
+//import it.unibo.bls.interfaces.IControlLed;
+//import it.unibo.bls.interfaces.IObserver;
+
+public interface IApplListener extends IObserver{
+    public void setControl(IAppLogic ctrl);
+    public int getNumOfClicks();
+}
