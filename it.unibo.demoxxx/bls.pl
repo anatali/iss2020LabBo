255cf48bae8f457a84676bc5a8519d4e7e5b3353
Antonio Natali <antonio.natali@unibo.it>
2020-04-27 19:10:32 +0200

    events in sible ctx and in more ctx

--- it.unibo.demoxxx/bls.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% bls description   
-%====================================================================================
-context(ctxbutton, "localhost",  "TCP", "8074").
-context(ctxbls, "127.0.0.1",  "TCP", "8095").
- qactor( led, ctxbls, "it.unibo.led.Led").
-  qactor( button, ctxbutton, "it.unibo.button.Button").
-  qactor( main, ctxbutton, "it.unibo.main.Main").
-tracing.
--- it.unibo.demoxxx/caller2.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% caller2 description   
-%====================================================================================
-mqttBroker("broker.hivemq.com", "1883").
-context(ctxres2, "localhost",  "TCP", "8048").
-context(ctxcaller2, "127.0.0.1",  "TCP", "8045").
- qactor( resource, ctxres2, "external").
-  qactor( caller2, ctxcaller2, "it.unibo.caller2.Caller2").
-msglogging.
