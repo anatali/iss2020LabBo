ca9568438df433274d894d21193c907564fe619b
anatalibo <antonio.natali@unibo.it>
2020-07-27 12:19:19 +0200

    webflux.intro

---
+++ webflux.intro/bin/default/demo/ReadMe.txt
@@ -0,0 +1,4 @@
+From:
+
+https://howtodoinjava.com/spring-webflux/spring-webflux-tutorial/#:~:text=It%20is%20fully%20non%2Dblocking,fully%20functional%20hello%20world%20example.
+
---
+++ webflux.intro/src/main/it/unibo/webflux/intro/Application.java
@@ -0,0 +1,23 @@
+package it.unibo.webflux.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	/*
+	 @Bean
+	    public SecurityWebFilterChain functionalValidationsSpringSecurityFilterChain(ServerHttpSecurity http) {
+	        http.authorizeExchange()
+	            .anyExchange()
+	            .permitAll();
+	        http.csrf().disable();
+	        return http.build();
+	    }	
+	    */
+}
