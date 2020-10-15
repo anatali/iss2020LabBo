9c671095df77bcb2d40d1709dc60aca56cc6f301
anatalibo <antonio.natali@unibo.it>
2020-10-15 11:41:15 +0200

    meeting 16 Oct

--- it.unibo.qak20.tearoom/tearoomsubsys.pl
+++ it.unibo.qak20.tearoom/tearoomsubsys.pl
@@ -2,4 +2,6 @@
 % tearoomsubsys description   
 %====================================================================================
 context(ctxtearoom_dummy, "localhost",  "TCP", "8050").
- qactor( tearoom, ctxtearoom_dummy, "it.unibo.tearoom.Tearoom").
+context(ctxbasicrobot, "192.168.1.68",  "TCP", "8020").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( waiterwalker, ctxtearoom_dummy, "it.unibo.waiterwalker.Waiterwalker").
